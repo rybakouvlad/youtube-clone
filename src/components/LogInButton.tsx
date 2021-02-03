e6dba1ed4a5cc2c836db30cff9217f81753f8098
rybakouvlad <rybakou.vladik@gmail.com>
2021-02-03 18:33:58 +0000

    Revert "Fix login"

--- src/components/LogInButton.tsx
+++
@@ -1,6 +0,0 @@
-import { Nav } from 'react-bootstrap';
-import React from 'react';
-
-export const LogInButton = () => {
-  return <Nav.Link href="/auth">Login</Nav.Link>;
-};
--- src/typings/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { IUser } from '../MongoDB/models/Users';
-declare global {
-  namespace Express {
-    interface Request {
-      user: IUser;
-    }
-  }
-}
